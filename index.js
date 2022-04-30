4a6fadedddf3e8db48fe873b7b330daf753e7ebb
IonSibRe <ionsibre@gmail.com>
2022-04-30 11:16:18 +0200

    added menu

--- index.js
+++ index.js
@@ -112,3 +112,146 @@ if (auto) {
 		addActiveDotClass();
 	}, intervalTime);
 }
+
+// ================ Menu ================
+const homeMenuSelectionContent = document.querySelector(
+	".home-menu-selection-content"
+);
+const menuBtns = document.querySelectorAll(".home-menu-selection-btn");
+const mealsList = [];
+
+const getMeals = async () => {
+	const response = await fetch("./meals.json");
+	const meals = await response.json();
+
+	for (const key in meals) mealsList.push(meals[key]);
+
+	displayMeals();
+};
+
+const displayMeals = () => {
+	mealsList.forEach((meal, index) => {
+		const domItem = document.createElement("div");
+		domItem.className = "home-menu-selection-item";
+		domItem.setAttribute("data-tags", meal.tags);
+		domItem.innerHTML = `
+            <div class="home-menu-selection-item-img-wrap">
+                    <img src="./img/menu_${index + 1}.jpg" alt="" />
+            </div>
+            <div
+                class="home-menu-selection-item-content-wrap"
+            >
+                <h3
+                    class="home-menu-section-item-content-title"
+                >
+                    ${meal.name}
+                </h3>
+                <div
+                    class="home-menu-selection-item-content-list"
+                >
+                    <div
+                        class="home-menu-selection-item-content-list-column"
+                    >
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Juice Fruit
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Vegetable
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Tomato
+                        </p>
+                    </div>
+                    <div
+                        class="home-menu-selection-item-content-list-column"
+                    >
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Chicken
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Milk
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Chilli
+                        </p>
+                    </div>
+                    <div
+                        class="home-menu-selection-item-content-list-column"
+                    >
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Cherry
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Potato
+                        </p>
+                        <p
+                            class="home-menu-selection-item-content-list-text"
+                        >
+                            - Sesame
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="home-menu-selection-item-price-wrap"
+            >
+                <h4 class="home-menu-selection-item-price">
+                    $20.00
+                </h4>
+            </div>
+        `;
+
+		homeMenuSelectionContent.appendChild(domItem);
+	});
+};
+
+const filterMeals = (tagParam) => {
+	let tag = tagParam.toLowerCase();
+	const items = [...homeMenuSelectionContent.children];
+
+	if (tag === "all") {
+		items.forEach((item) => (item.style.display = "flex"));
+	} else {
+		items.forEach((item) => {
+			if (item.getAttribute("data-tags").split(",").includes(tag)) {
+				item.style.display = "flex";
+			} else {
+				item.style.display = "none";
+			}
+		});
+	}
+};
+
+menuBtns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		// Clear and set active class
+		menuBtns.forEach((btn) => {
+			if (btn.classList.contains("home-menu-selection-btn-active")) {
+				btn.classList.remove("home-menu-selection-btn-active");
+			}
+		});
+
+		btn.classList.add("home-menu-selection-btn-active");
+
+		// Filter Meals
+		filterMeals(btn.textContent.trim());
+	});
+});
+
+getMeals();
